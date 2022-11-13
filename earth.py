cb68ec59ba99712fe70b83b185fd85af5fcdc020
Aryenne Marie Oliveros <oliveros.aryenne@gmail.com>
2022-11-13 01:48:58 +0000

    Added the next for the ocean but text doesnt fully disappear when not hovering over the button

--- earth.py
+++ earth.py
@@ -5,6 +5,23 @@ pygame.init()
 
 SIZE = 720, 640
 screen = pygame.display.set_mode(SIZE)
+def create_beach_bg():
+    # Create the sea
+    sea = Rect(0, 0, 720, 280)
+    pygame.draw.rect(screen, "#2700ff", sea)
+    #
+    sand = Rect(0, 280, 720, 360)
+    pygame.draw.rect(screen, "#ffeb38", sand)
+
+def runBeach():
+    beach = True
+    while beach:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                beach = False
+
+        create_beach_bg()
+        pygame.display.flip()
 
 def create_earth():
     img = pygame.image.load("earth.png")
@@ -24,12 +41,25 @@ def locations():
     for i in range(0, 3):
         pygame.draw.circle(screen, red, (coord[i][0], coord[i][1]), 15)
 
-    # Making the circle interactive
+    # Making the circle interactive when hovering
     mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    # Create the text for each location
+    oceanText = pygame.image.load("ocean-text.png")
+    # beachText = pygame.image.load("beach.text.png")
+    # riverText = pygame.image.load("river-text.png")
+
     # Checking if the mouse if hovering over the circle
     if (mouse[0] >= coord[0][0]-15 and mouse[0] <= coord[0][0]+15)\
             and (mouse[1] >= coord[0][1]-15 and mouse[1] <= coord[0][1]+15):
         pygame.draw.circle(screen, darkRed, (coord[0][0], coord[0][1]), 15)
+        screen.blit(oceanText, (360, 320))
+        if click[0] == 1:
+            runBeach()
+    else:
+        pygame.transform.scale(oceanText, (0, 0))
+
 
     if (mouse[0] >= coord[1][0]-15 and mouse[0] <= coord[1][0]+15)\
             and (mouse[1] >= coord[1][1]-15 and mouse[1] <= coord[1][1]+15):
@@ -39,7 +69,9 @@ def locations():
             and (mouse[1] >= coord[2][1]-15 and mouse[1] <= coord[2][1]+15):
         pygame.draw.circle(screen, darkRed, (coord[2][0], coord[2][1]), 15)
 
+    # When the buttons get clicked
 
+# Main
 game = True
 while game:
     for event in pygame.event.get():
